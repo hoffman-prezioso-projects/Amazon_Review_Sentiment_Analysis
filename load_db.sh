c1cb9d4be7cb3c49f154191758f659c5db08da33
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-04-05 12:15:08 -0400

    Add script to load file into sqlite3

---
+++ load_db.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "No data file provided"
+	echo "Use: $0 <data filename>"
+	exit 0
+fi
+
+DATABASE="sentiment.db"
+TABLE="data"
+
+sqlite3 $DATABASE <<EOS
+
+DROP TABLE IF EXISTS $TABLE;
+CREATE TABLE $TABLE (word TEXT, sentiment REAL);
+
+.mode csv
+.separator "\t"
+.import $1 $TABLE
+
+EOS
