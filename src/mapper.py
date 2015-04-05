e62b27ba9847c4f5495467eb15c2c412aad3dfdc
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-04-05 08:39:29 -0400

    Fix bug in mapper and emit ntf

--- src/mapper.py
+++ src/mapper.py
@@ -3,31 +3,40 @@
 import re
 import sys
 
-splitter = re.compile('\s')
+splitter = re.compile('\s+')
+nonword_pattern = re.compile(r'[^a-zA-Z]')
 
 def get_word_freq(text):
-	word_req = {};
-	words = splitter.split(text)
-	for word in words:
-		if word in word_req:
-			word_req[word] += 1
-		else:
-			word_req[word] = 1
-	return word_req
+  word_freq = {};
+  max_term_freq = 0
+  words = splitter.split(text)
+  for word in words:
+    word = word.lower()
+    if word != '':
+      if word in word_freq:
+        word_freq[word] += 1
+      else:
+        word_freq[word] = 1
+      if word_freq[word] > max_term_freq:
+        max_term_freq = word_freq[word]
+  return word_freq, max_term_freq
 
 review = {}
 
 for line in sys.stdin:
-	
-	line = line.strip()
-	key, value = line.split(': ', 1)
+  
+  line = line.strip()
+  key, value = line.split(': ', 1)
 
-	review[key] = value
+  review[key] = value
 
-	if key == 'review/text':
-		word_freq = get_word_freq(value)
-		for word in word_freq:
-			print '%s\t%s,%s' % (word, review['review/score'], word_freq[word])
-		review = {}
+  if key == 'review/text':
+    words = re.sub(nonword_pattern, ' ', value)
+    word_freq, max_term_freq = get_word_freq(words)
+    
+    for word in word_freq:
+      # emit word, rating, ntf, and frequency
+      print '%s\t%s\t%s\t%s' % (word, review['review/score'], word_freq[word] / float(max_term_freq), word_freq[word])
+    review = {}
 
 
