142c3c0ff28cd19b4594feb11deb353d538977c8
Stefano Prezioso <stefprez@gmail.com>
2015-04-13 19:48:04 -0400

    Streamlined reducer

--- src/reducer.py
+++ src/reducer.py
@@ -1,39 +1,29 @@
 #!/usr/bin/python
 
 from decimal import *
-import math
 import sys
 
-# import entropy
 
-
-def emit(word, occurrences):
-    # total_occurrences = sum(occurrences)
-    # total_rating = sum([a*b for a,b in zip(occurrences, range(1,6))])
-    # average = float(total_rating) / total_occurrences
-    # print '%s\t%s\t%s\t%s' % (word, average, total_occurrences, entropy.entropy(occurrences))
-    print '%s\t%s' % (word, '\t'.join(str(x) for x in occurrences))
+def emit(word, occurrence_counts):
+    print '%s\t%s' % (word, '\t'.join(str(x) for x in occurrence_counts))
 
 current_word = None
-
-reviews = [0] * 5
+review_total_counts = [0] * 5
 
 for line in sys.stdin:
     word, rating, occurrences = line.strip().split('\t', 2)
-    rating = int(float(rating))
+    rating = int(rating)
     occurrences = int(occurrences)
 
-    reviews[rating - 1] += occurrences
+    review_total_counts[rating - 1] += occurrences
 
     if word != current_word:
         if current_word:
-            emit(current_word, occurrence_count)
+            emit(current_word, occurrence_counts)
 
         current_word = word
-        occurrence_count = [0] * 5
-        doc_count = [0] * 5
+        occurrence_counts = [0] * 5
 
-    occurrence_count[rating - 1] += occurrences
-    doc_count[rating - 1] += 1
+    occurrence_counts[rating - 1] += occurrences
 
-emit(word, occurrence_count)
+emit(word, occurrence_counts)
