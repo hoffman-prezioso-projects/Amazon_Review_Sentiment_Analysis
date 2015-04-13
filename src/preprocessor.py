d499797d0249fe6748715cee460a1081a44aed0d
Stefano Prezioso <stefprez@gmail.com>
2015-04-12 23:25:00 -0400

    Have preprocessor strip out unneeded lines. Increased reviews per file to 100,000.

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 
 import time
+import sys
 
 print "This program splits Amazon reviews file into individual .review files."
 print "These are stored in a directory created in your current directory."
-master_file_path = raw_input("Enter master file path: ")
+# master_file_path = raw_input("Enter master file path: ")
 start_time = time.time()
-
+print sys.argv[1]
 try:
-    master_file = open(master_file_path)
+    master_file = open(sys.argv[1])
 except:
     print "Invalid file:", master_file_path
     exit(0)
 
 
 record_counter = 0
-reviews_per_file = 10000
+reviews_per_file = 100000
 record_file_directory = "./reviews/"
 current_record_file = open(record_file_directory +
                            str(record_counter).zfill(8) + ".review", 'w')
@@ -23,12 +24,14 @@ current_record_contents = ""
 
 for line in master_file:
     if not line.isspace():
-        current_record_contents += line
+        if line.startswith("review/score") or line.startswith("review/text"):
+            current_record_contents += line
     else:
         record_counter += 1
         if record_counter % reviews_per_file == 0:
             current_record_file.write(current_record_contents)
             current_record_file.close()
+            print "%d reviews processed" % (record_counter)
             current_record_file = open(record_file_directory +
                                        str(record_counter / reviews_per_file)
                                        .zfill(8) + ".review", 'w')
@@ -36,4 +39,7 @@ for line in master_file:
         else:
             current_record_contents += line
 
+current_record_file.write(current_record_contents)
+current_record_file.close()
+print "%d reviews processed" % (record_counter)
 print "Finished in: %s seconds" % (time.time() - start_time)
