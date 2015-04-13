82a9b80a164f0db226a12f27c630d6b717110a0c
Stefano Prezioso <stefprez@gmail.com>
2015-04-13 19:32:07 -0400

    Updated mapper efficiency, added spell checker

--- src/mapper.py
+++ src/mapper.py
@@ -2,33 +2,31 @@
 
 import re
 import sys
+from spell_checker import SpellChecker
 
-splitter = re.compile('\s+')
 nonword_pattern = re.compile(r'[^a-zA-Z]')
+sc = SpellChecker()
 
 
 def get_word_freq(text):
     word_freq = {}
-    max_term_freq = 0
-    words = splitter.split(text)
+    words = text.split()
+
     for word in words:
         word = word.lower()
-        if word != '':
+        if (sc.check(word)):
             if word in word_freq:
                 word_freq[word] += 1
             else:
                 word_freq[word] = 1
-            if word_freq[word] > max_term_freq:
-                max_term_freq = word_freq[word]
-    return word_freq, max_term_freq, len(words)
 
+    max_term_freq = 0
 
-def emit(words, review, source):
-    word_freq, max_term_freq, num_words = get_word_freq(words)
     for word in word_freq:
-        count = word_freq[word]
-        print '%s\t%s\t%s' % (word, review['review/score'], count)
+        if word_freq[word] > max_term_freq:
+            max_term_freq = word_freq[word]
 
+    return word_freq, max_term_freq
 
 review = {}
 
@@ -40,15 +38,16 @@ for line in sys.stdin:
     except ValueError:
         continue
 
-    if key == 'product/productId':
-        review = {}
-
     review[key] = value
 
-    if key == 'review/summary':
+    if key == 'review/text':
         words = re.sub(nonword_pattern, ' ', value)
-        emit(words, review, 'summary')
 
-    '''elif key == 'review/text':
-        words = re.sub(nonword_pattern, ' ', value)
-        emit(words, review, 'text')'''
+        word_freq, max_term_freq = get_word_freq(words)
+
+        for word in word_freq:
+            # emit word, rating, ntf, and frequency
+            print '%s\t%s\t%s\t%s' % (word, review['review/score'],
+                                      word_freq[word] / float(max_term_freq),
+                                      word_freq[word])
+        review = {}
