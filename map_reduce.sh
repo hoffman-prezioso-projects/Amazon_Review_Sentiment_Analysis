701c53b206517d41765b06f41c01f237df5a137d
Stefano Prezioso <stefprez@gmail.com>
2015-04-14 00:31:26 -0400

    Added timestamps to map_reduce script.

--- map_reduce.sh
+++ map_reduce.sh
@@ -1,18 +1,28 @@
 OUTPUT_DIR="output"
 
+date
+
 echo "Creating crawl directory..."
 hadoop fs -mkdir crawl > /dev/null 2>&1
+
+date
 
 echo "Creating reviews directory..."
 hadoop fs -mkdir crawl/reviews > /dev/null 2>&1
 hadoop fs -rm crawl/reviews/* > /dev/null 2>&1
 
+date
+
 echo "Removing previous output..."
 hadoop fs -rm -r crawl/$OUTPUT_DIR > /dev/null 2>&1
+
+date
 
 echo "Copying reviews..."
 hadoop dfs -copyFromLocal reviews crawl > /dev/null 2>&1
 echo "Copy complete!"
+
+date
 
 echo "Running mapReduce..."
 hadoop jar /usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming.jar \
@@ -23,6 +33,8 @@ hadoop jar /usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming.jar
 -output crawl/$OUTPUT_DIR
 echo "mapReduce complete!"
 
+date
+
 # create local output directory
 if [ ! -d $OUTPUT_DIR ]; then
 	mkdir $OUTPUT_DIR
@@ -32,3 +44,5 @@ fi
 
 echo "Creating output directory"
 hadoop dfs -copyToLocal crawl/$OUTPUT_DIR ./
+
+date
